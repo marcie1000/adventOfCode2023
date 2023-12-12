a5512fe54131f5efe441f546f7cdd62254d82afc
Adelia B <adelia.baudn@gmail.com>
2023-12-12 23:19:03 +0100

    Day 5 part 2
    renamed folders

--- day5/main.py
+++ day5/main.py
@@ -11,8 +11,8 @@ def findall_int(needle, haystack):
         l[i] = int(l[i])
     return l
 
-def getSublist(needle, txt, line):
-    assert(re.findall(needle, txt[line]))
+def getSublist(txt, line):
+    #assert(re.findall(needle, txt[line]))
     sublist = []
     while(line < len(txt) and txt[line] != "\n"):
         sublist.append(findall_int(r"\d+", txt[line]))
@@ -31,40 +31,85 @@ def convert(lst, mapl):
         it += 1
     return lst
 
-txt = readfile()
-line = 0
+def part1():
+    print("Part 1: ", end="")
+    txt = readfile()
+    line = 0
+    # read seeds list
+    seeds = findall_int(r"\d+", txt[line])
+    line += 2
+    for i in range(7):
+        mapp, line = getSublist(txt, line)
+        seeds = convert(seeds, mapp)
 
-# read seeds list
+    print("The lowest location number is:", min(seeds))
 
-seeds = findall_int(r"\d+", txt[line])
+def testKeyOut(li):
+    return(li[0])
 
-line += 2
+def testKeyIn(li):
+    return(li[1])
 
-# Parse maps
+# Test every couple start of range/length of range in every map
+# Modify values in couples through maps
+# Subdivide couples if they don't fit in a single map bounds
+def test(parsed, cpl):
+    sortedIn = parsed.copy()
+    sortedOut = parsed.copy()
+    sortedIn.sort(key=testKeyIn)
+    sortedOut.sort(key=testKeyOut)
+    if sortedIn[0][1] > 0:
+        sortedOut = [[0, 0, sortedIn[0][1]]] + sortedOut
+        sortedIn = [[0, 0, sortedIn[0][1]]] + sortedIn
+    minmax = []
+    for m in sortedIn:
+        minmax.append([m[1], m[1] + m[2] - 1, m[0], m[0] + m[2] - 1, - (m[1] - m[0])])
 
-seedsToSoil, line = getSublist("seed-to-soil map:", txt, line)
-soilToFertilizer, line = getSublist("soil-to-fertilizer map:", txt, line)
-fertilizerToWater, line = getSublist("fertilizer-to-water map:", txt, line)
-waterToLight, line = getSublist("water-to-light map:", txt, line)
-lightToTemperature, line = getSublist("light-to-temperature map:", txt, line)
-temperatureToHumidity, line = getSublist("temperature-to-humidity map:", txt, line)
-humidityToLocation, line = getSublist("humidity-to-location map:", txt, line)
+    i = 0
+    for c in cpl:
+        cplmin = c[0]
+        cplmax = c[0] + c[1] - 1
+        j = 0
+        found = False
+        while(not found and j < len(minmax)):
+            minIn = minmax[j][0]
+            maxIn = minmax[j][1]
+            minOut = minmax[j][2]
+            maxOut = minmax[j][3]
+            delta = minmax[j][4]
+            if(cplmin >= minIn and cplmin <= maxIn):
+                cpl[i][0] += delta
+                found = True
+                if(cplmax > maxIn):
+                    cpl[i][1] = maxIn - cplmin
+                    cpl.append([maxIn + 1, cplmax - maxIn])
+            j += 1
+        i += 1
+    return(cpl)
 
-# Convert setp by step
+def part2():
+    print("Part 2: ", end="")
+    txt = readfile()
+    line = 0
+    # read seeds list
+    seeds = findall_int(r"\d+", txt[line])
+    cpl = []
+    for i in range(0, len(seeds), 2):
+        cpl.append([seeds[i], seeds[i + 1]])
+    line += 2
 
-loc = seeds.copy()
-loc = convert(loc, seedsToSoil)
-loc = convert(loc, soilToFertilizer)
-loc = convert(loc, fertilizerToWater)
-loc = convert(loc, waterToLight)
-loc = convert(loc, lightToTemperature)
-loc = convert(loc, temperatureToHumidity)
-loc = convert(loc, humidityToLocation)
+    # Parse maps
+    for i in range(7):
+        mapp, line = getSublist(txt, line)
+        cpl = test(mapp, cpl)
 
-print("The lowest location number is:", min(loc))
+    mincpl = []
+    for c in cpl:
+        mincpl.append(c[0])
 
+    print("The lowest location number is:", min(mincpl))
 
-
-
-
+if(__name__ == "__main__"):
+    part1()
+    part2()
 
