9ca8bed43a38f64e0ec24c4b28bf28429f2e3c6e
Adelia B <adelia.baudn@gmail.com>
2023-12-28 00:15:17 +0100

    Day10 part 2

--- day10/main.cpp
+++ day10/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <array>
 #include <algorithm>
 
 enum tiles {
@@ -122,27 +123,147 @@ bool checkDestTile(int &sx, int &sy, std::vector<std::string> &doc, int dir)
     return false;
 }
 
-bool searchPath(int &sx, int &sy, std::vector<std::string> &doc)
+bool replacebounds(int &x, int &y, std::vector<std::string> &doc)
+{
+    bool correct = true;
+    if(x < 0)
+    {
+        x = 0;
+        correct = false;
+    }
+    if(y < 0)
+    {
+        y = 0;
+        correct = false;
+    }
+    if(x >= (int) doc[0].size())
+    {
+        x = doc[0].size() - 1;
+        correct = false;
+    }
+    if(y >= (int) doc.size())
+    {
+        y = doc.size() - 1;
+        correct = false;
+    }
+    return correct;
+}
+
+void replaceEnclosedDots(int sx, int sy, std::vector<std::string> &doc, int dir, int &nb, std::vector<std::array<int, 2>> &iPos, bool left)
+{
+    int sxleft { sx }, sxright { sx }, syleft { sy }, syright { sy };
+
+    switch(dir)
+    {
+        case NORTH:
+            sxleft--;
+            sxright++;
+            break;
+        case SOUTH:
+            sxleft++;
+            sxright--;
+            break;
+        case EAST:
+            syleft--;
+            syright++;
+            break;
+        case WEST:
+            syleft++;
+            syright--;
+            break;
+    }
+    
+    replacebounds(sxleft, syleft, doc);
+    replacebounds(sxright, syright, doc);
+    
+    if(left)
+    {
+        if(doc[syleft][sxleft] == '.')
+        {
+            nb++;
+            doc[syleft][sxleft] = 'I';
+            iPos.push_back({ syleft, sxleft });
+        }
+    }
+    else
+    {
+        if(doc[syright][sxright] == '.')
+        {
+            nb++;
+            doc[syright][sxright] = 'I';
+            iPos.push_back({ syright, sxright });
+        }
+    }
+}
+
+bool searchPath(int &sx, int &sy, std::vector<std::string> &doc, int &nb, std::vector<std::array<int, 2>> &iPos, bool left)
 {
     int dir;
+    int prevX { sx }, prevY{ sy };
     for(dir = 0; dir < 4; dir++)
     {
-        int prevX { sx }, prevY{ sy };
+        
         if(checkDir(sx, sy, doc, dir) and (checkDestTile(sx, sy, doc, dir)))
         {
-            doc[prevY][prevX] = '.';
-//            std::cout << doc[prevY] << std::endl;
+            doc[prevY][prevX] = ' ';
+            replaceEnclosedDots(sx, sy, doc, dir, nb, iPos, left);
+            replaceEnclosedDots(prevX, prevY, doc, dir, nb, iPos, left);
             return true;
         }
     }
+    doc[prevY][prevX] = ' ';
     return false;
+}
+
+// Used by completeEnclosed
+void completeEnclosedPos(std::vector<std::string> &doc, int &nb, std::vector<std::array<int, 2>> &iPos, int ix, int iy, int i)
+{
+    int y { iPos[i][0] }, x { iPos[i][1] };
+    if(ix == 0 and iy == 0)
+        return;
+    x += ix;
+    y += iy;
+    if(!replacebounds(x, y, doc))
+        return;
+    if(doc[y][x] != ' ' and doc[y][x] != 'I')
+    {
+        doc[y][x] = 'I';
+        iPos.push_back({y, x});
+        nb++;
+    }
+}
+
+// Look around every 'I' tiles (enclosed tiles that have been
+// counted during path search). If a '.' is found near an 'I', 
+// counter is incremented and this new tile becomes 'I'.
+void completeEnclosed(std::vector<std::string> &doc, int &nb, std::vector<std::array<int, 2>> &iPos)
+{
+    /*
+     * Looks if '?' tiles are dots
+     * 
+     * *?* 
+     * ?I?
+     * *?*
+     * 
+     */
+    
+    
+    for(int i = 0; i < (int) iPos.size(); i++)
+    {
+        completeEnclosedPos(doc, nb, iPos, -1,  0, i);
+        completeEnclosedPos(doc, nb, iPos,  1,  0, i);
+        completeEnclosedPos(doc, nb, iPos,  0, -1, i);
+        completeEnclosedPos(doc, nb, iPos,  0,  1, i);
+    }
 }
 
 int main(void)
 {
     std::ifstream in { "input.txt" };
     std::string s; // single line
-    std::vector<std::string> doc; 
+    std::vector<std::string> doc, refl, refr; 
+    // List of positions of enclosed tiles
+    std::vector<std::array<int, 2>> iPosl, iPosr;
     
     if(!in.is_open())
     {
@@ -150,13 +271,12 @@ int main(void)
         return(1);
     }
     
+    // Read file and search 'S' position
     int sxInit { 0 }, syInit { 0 };
     int i = 0;
     while(std::getline(in, s) and s != "")
     {
-//        std::cout << s << std::endl;;
         doc.push_back(s);
-        // search 's'
         auto const fnd { std::find(std::begin(s), std::end(s), 'S') };
         if(fnd != std::end(s))
         {
@@ -166,25 +286,70 @@ int main(void)
         i++;
     }
     
-//    std::cout << sxInit << " " << syInit << std::endl;
+    std::copy(std::begin(doc), std::end(doc), std::back_inserter(refl));
     
     i = 0;
     int sx { sxInit }, sy { syInit };
-    while(searchPath(sx, sy, doc))
-    {
+    int nbl { 0 }, nbr { 0 };
+    // Initial search
+    // here, iPosl contains incorrect values
+    while(searchPath(sx, sy, doc, nbl, iPosl, true))
         i++;
-        
-//        for(auto const &e : doc)
-//        {
-//            std::cout << e << std::endl;
-//        }
-//        std::cout << std::endl << std::endl;
+    i++;
+    
+    // Delete every pipes that are not in the path
+    for(int j = 0; j < (int) doc.size(); j++)
+    {
+        for(int k = 0; k < (int) doc[j].size(); k++)
+        {
+            if(doc[j][k] != ' ')
+                refl[j][k] = '.';
+        }
+    }
+    
+    // Does another research of path to count the enclosed tiles
+    // Two counts will be done: one looking to the left, one looking to the right
+    // refl and refr are like the initial grid, but all pipes that are not in
+    // the path are replaced by '.'
+    
+    std::copy(std::begin(refl), std::end(refl), std::back_inserter(refr));
+    
+    // clear because incorrect values
+    iPosl.clear();
+    
+    i = 0;
+    int sxl { sxInit }, syl { syInit };
+    int sxr { sxInit }, syr { syInit };
+    nbl = 0;
+    nbr = 0;
+    while(searchPath(sxl, syl, refl, nbl, iPosl, true))
+    {
+        searchPath(sxr, syr, refr, nbr, iPosr, false);
+        i++;
     }
     
     i++;
     
-    std::cout << "path size : " << i << std::endl;
-    std::cout << "farthest point : " << i / 2 << "\n";
+    std::cout << "Path size : " << i << std::endl;
+    std::cout << "\nPART 1: farthest point : " << i / 2 << "\n";
+    
+    // Look around every 'I' tiles (enclosed tiles that have been
+    // counted during path search). If a '.' is found near an 'I', 
+    // counter is incremented and this new tile becomes 'I'.
+    completeEnclosed(refl, nbl, iPosl);
+    completeEnclosed(refr, nbr, iPosr);
+
+    std::cout << "\nNumber of enclosed tiles (looking to the left): " << nbl << "\n";
+    std::cout << "Number of enclosed tiles (looking to the right): " << nbr << "\n";
+    
+    // Keeps the lowest value, because the number of enclosed tiles is probably lower
+    // than the number of tiles outside the loop. If the final answer isn't correct,
+    // maybe the other answer should be tried.
+    std::cout << "\nPART 2: the answer is (probably): ";
+    if(nbl < nbr)
+        std::cout << nbl << "\n";
+    else 
+        std::cout << nbr << "\n";
     
     return 0;
 }
